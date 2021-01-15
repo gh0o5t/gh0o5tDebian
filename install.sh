adf5b0d5b8611ed7a45f783528d99a8f0d22ee3e
gh0o5t <gh0o5t@protonmail.com>
2021-01-15 21:33:27 +0100

    Brave installation mod

--- install.sh
+++ install.sh
@@ -55,7 +55,8 @@ ln -sf ~/.local/share/themes ~/.themes
 # Install Brave Browser
 run_command "curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -"
 run_command "echo 'deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list"
-run_command "sudo apt update && sudo apt install brave-browser"
+sudo apt update && sudo apt install -y brave-browser
+check_command "Installation of brave browser failed"
 
 # Vim plugins
 run_command "curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
